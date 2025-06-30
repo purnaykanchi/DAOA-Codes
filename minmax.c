c6da58671b30eafdfc01eba522b252d000f63a0e
purnaykanchi <padmamddph@gmail.com>
2025-06-30 20:23:20 +0530

    Update minmax.c

--- minmax.c
+++ minmax.c
@@ -1,30 +1,22 @@
 #include <stdio.h>
 int max, min;
 int arr[100];
-void maxmin(int i, int j)
-{
+void maxmin(int i, int j){
     int max1, min1, mid;
-    if (i == j)
-    {
+    if (i == j){
         max = min = arr[i];
     }
-    else
-    {
-        if (i == j - 1)
-        {
-            if (arr[i] < arr[j])
-            {
+    else{
+        if (i == j - 1){
+            if (arr[i] < arr[j]){
                 max = arr[j];
                 min = arr[i];
             }
-            else
-            {
+            else{
                 max = arr[i];
                 min = arr[j];
             }
-        }
-        else
-        {
+        }else{
             mid = (i + j) / 2;
             maxmin(i, mid);
             max1 = max;
@@ -37,20 +29,16 @@ void maxmin(int i, int j)
         }
     }
 }
-
-int main()
-{
+int main(){
     int n;
     printf("Enter the number of elements in the array: ");
     scanf("%d", &n);
-    for (int i = 1; i <= n; i++)
-    {
+    for (int i = 1; i <= n; i++){
         printf("Enter the element: ");
         scanf("%d", &arr[i]);
     }
     printf("The input array is:\t");
-    for (int i = 1; i <= n; i++)
-    {
+    for (int i = 1; i <= n; i++){
         printf("%d\t", arr[i]);
     }
     min = arr[0];
